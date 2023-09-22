740d577da739a55102c951c4e2bf7891fad2a90e
Burak <brkkcygt32@gmail.com>
2023-09-22 16:27:24 +0300

    revise with php and bootstrap

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                </a>
+
+                <h1>My Todo List</h1>
+
+                <div class="text-end">
+                    <a href="login.php"><button type="button" class="btn btn-outline-light me-2" onclick="login.php">
+                            Login
+                        </button></a>
+                    <a href="register.php"><button type="button" class="btn btn-warning">Sign-up</button></a>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
